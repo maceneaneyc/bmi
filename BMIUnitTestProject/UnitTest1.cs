c7ecb88e05840fdbda7d6eb05ad81660c65a59d0
Catherine MacEneaney <X00168288@myTUDublin.ie>
2019-11-10 16:17:29 +0000

    BMI Unit Tests

--- BMIUnitTestProject/UnitTest1.cs
+++ BMIUnitTestProject/UnitTest1.cs
@@ -1,17 +1,65 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using BMICalculator;
+using System;
 
 namespace BMIUnitTestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestBMICategory
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestUnderweightBMICategoryMethod()
         {
+            //Arrange
+            BMI bmi = new BMI() { WeightStones = 6, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            int expectedResult = 0;
+
+            //Act
+            int actualResult = Convert.ToInt32(bmi.BMICategory);
+
+            //Assert
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+        [TestMethod]
+        public void TestNormalBMICategoryMethod()
+        {
+            //Arrange
             BMI bmi = new BMI() { WeightStones = 12, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
-            Assert.AreEqual(bmi.BMICategory, BMICategory.Normal);
+            int expectedResult = 1;
+
+            //Act
+            int actualResult = Convert.ToInt32(bmi.BMICategory);
+
+            //Assert
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestOverweightBMICategoryMethod()
+        {
+            //Arrange
+            BMI bmi = new BMI() { WeightStones = 14, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            int expectedResult = 2;
+
+            //Act
+            int actualResult = Convert.ToInt32(bmi.BMICategory);
+
+            //Assert
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [TestMethod]
+        public void TestObeseBMICategoryMethod()
+        {
+            //Arrange
+            BMI bmi = new BMI() { WeightStones = 16, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            int expectedResult = 3;
+
+            //Act
+            int actualResult = Convert.ToInt32(bmi.BMICategory);
+
+            //Assert
+            Assert.AreEqual(actualResult, expectedResult);
         }
     }
 }
